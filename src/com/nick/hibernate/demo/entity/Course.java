ec99b32f196d61735265c3a2e927018dfd469d95
Nikhil Joseph Peter <josef.nikhil18@gmail.com>
2020-09-07 17:49:43 +0530

    table commit

---
+++ src/com/nick/hibernate/demo/entity/Course.java
@@ -0,0 +1,60 @@
+package com.nick.hibernate.demo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "course")
+@NoArgsConstructor
+@Data
+public class Course {
+
+	public Course(String title) {
+		super();
+		this.title = title;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "title")
+	private String title;
+
+	@ManyToOne(cascade = {CascadeType.DETACH,CascadeType.MERGE,
+			CascadeType.PERSIST, CascadeType.REFRESH})
+	@JoinColumn(name = "instructor_id")
+	private Instructor instructor;
+	
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
+
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", title=" + title + "]";
+	}
+	
+	public void addReview(Review review) {
+		if(review == null) {
+			this.reviews = new ArrayList<>();
+		}		
+		this.reviews.add(review);
+	}
+}
